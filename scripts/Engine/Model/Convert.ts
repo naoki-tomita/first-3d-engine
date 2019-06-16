f6db402caf2832bfceaee31ea53fdb4ade870ead
naoki.tomita <mxcpy608@hotmail.com>
2019-06-16 16:55:02 +0900

    Improve comment.

--- scripts/Engine/Model/Convert.ts
+++ scripts/Engine/Model/Convert.ts
@@ -3,7 +3,10 @@ import { Face } from "../Face";
 import { Color } from "../Color";
 import { Model } from "../Model";
 
-export function convert(object: any) {
+export function convert(object: {
+  vertics: Array<{ x: number, y: number, z: number }>,
+  faces: number[][];
+}) {
   const v = object.vertics.map((v: any) => new Vertex3D(v.x, v.y, v.z));
   const f = object.faces.map((f: any) => new Face(v[f[0]], v[f[1]], v[f[2]], Color.orange));
   return new Model(v, f);
