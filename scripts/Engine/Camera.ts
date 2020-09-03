87dfd5a4e1f45695edcf8d663419da6546e60672
kojiro.ueda <mxcpy608@hotmail.com>
2020-09-03 21:04:52 +0900

    WIP

--- scripts/Engine/Camera.ts
+++ scripts/Engine/Camera.ts
@@ -43,22 +43,6 @@ export class PerspectiveCamera extends Camera {
     return new Vertex2D(ratio * (x - this.position.x), ratio * (y - this.position.y));
   }
 
-  project_(vertex : Vertex3D): Vertex2D {
-    // 向きのベクトルを得る
-    const dx = this.direction.x - this.position.x;
-    const dy = this.direction.y - this.position.y;
-    const dz = this.direction.z - this.position.z;
-    // 向きベクトルから、カメラの回転角を計算する
-    const theta = Math.atan2(dx, dy);
-    const phi = Math.atan2(dz, dx);
-    const psi = Math.atan2(dy, dz);
-    // カメラの回転角から、点の座標を逆回転させる => カメラの座標系に変換する
-    const rotatedPoint = Matrix.rotate(vertex, this.position, phi, theta, psi);
-
-    const ratio = (this.distance / ((rotatedPoint.z - this.position.z) || 1));
-    return new Vertex2D(ratio * (rotatedPoint.x - this.position.x), ratio * (rotatedPoint.y - this.position.y));
-  }
-
   culling(face: Face): boolean {
     return super.culling(face) && this.perspectiveCurring(face);
   }
